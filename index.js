84cff0fd54457aa692a3a63d85b2fc9a4d6564e8
Subkey <2822448396@qq.com>
2022-01-22 19:21:42 +0800

    feat: ii release

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 #!/usr/bin/env node
+const { exec } = require('child_process')
+const fs = require('fs')
+const path = require('path')
+
 let params = process.argv.slice(2)
-const get = (prm)=> {
+let root = process.cwd()
+const get = (prm) => {
   if(params.indexOf(prm)==-1) return undefined
   else return params[params.indexOf(prm) + 1]
+}
+const read = (file, encode=undefined) => {
+  let filepath = path.join(__dirname,file)
+  return fs.readFileSync(filepath, encode)
 }
 
 let cmd = params[0]
 let file = get('-h')
-console.log(process.cwd(),cmd,file)
+console.log(cmd,file)
+
+if(cmd==undefined || cmd=='-h' || cmd=='--help') {
+  let helpfile = read('docs/help.txt','utf-8')
+  console.log(helpfile)
+}
+
+else if(cmd=='release') {
+  exec('npm version patch && npm publish && git push --follow-tags',(err,stdout,stderr)=>{
+    if(err) {
+      console.log(err)
+    }else {
+      console.log(stdout)
+      console.log(stderr)
+    }
+  })
+}
+
+else if(cmd=='filetree') {
+
+}
+
+else {
+  console.error('ii: Unknown command')
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+# iipub
+easy push and publish your project online!
+## use
+`npm i iipub -g`
