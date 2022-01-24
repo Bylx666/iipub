c9f559ede7dc5984f31892d4d048c6f5ae5316cb
Subkey <2822448396@qq.com>
2022-01-25 00:45:06 +0800

    feat: read mime from file suffix

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const path = require('path')
 const rl = require('readline')
 const templates = require('./filetree/templates')
 const http = require('http')
+const eol = require('os').EOL
 
 let params = process.argv.slice(2)
 let root = process.cwd()
@@ -109,7 +110,7 @@ else if(cmd=='filetree') {
   const start = ()=>{
     if(gitignore) {
       try{
-        ignores = fs.readFileSync(path.join(folder,'.gitignore'),{encoding:'utf8'}).split(require('os').EOL) 
+        ignores = fs.readFileSync(path.join(folder,'.gitignore'),{encoding:'utf8'}).split(eol) 
       }catch(err){
         console.warn(`Ignore file not found: ${err.path}`)
       }
@@ -125,11 +126,43 @@ else if(cmd=='filetree') {
 }
 
 else if(cmd=='server') {
+  if(params.indexOf('--set-mime')!=-1) {
+    exec(path.join(__dirname,'server/mime.types'), (error,stdout,stderr)=>{
+      if(error) console.log(error)
+      else {
+        console.log(stdout)
+        console.log(stderr)
+      }
+    })
+    return
+  }
+
   let port = parseInt( get('-p') || get('--port') || 8088 )
   let acao = tf('-acao','--allow-cors','t') 
   let pd = tf('-pd','--print-detail','t')
   let index = get('-i') || get('--index') || 'index.html'
 
+  const getMime = ()=> {
+    let mimefile = read('server/mime.types','utf-8')
+      .replace(new RegExp(eol,'g'),'') // delete eols
+      .replace('types {','').replace('}','') // delete outside of {}
+      .replace(/ +/g,' ').replace(/(?<=;) /g,'').replace(/^ /,'') // cut spaces
+      .split(';')
+    let mimeList = []
+    mimefile.pop() // delete last empty object in array
+    mimefile.forEach((val)=>{
+      val = val.split(' ')
+      let dest = val[0]
+      val.shift()
+      mimeList.push({
+        dest:dest,
+        value:val
+      })
+    })
+    return mimeList
+  }
+  let mimes = getMime()
+  
   let server = http.createServer((req,res)=>{
     if(acao) res.setHeader('access-control-allow-origin','*')
     if(req.url=='/') req.url=index
@@ -140,7 +173,14 @@ else if(cmd=='server') {
     }else {
       try {
         let file = fs.readFileSync(path.join(root,req.url))
-        res.writeHead(200)
+        let fileSuffix = req.url.split('.').pop()
+        let contentType
+        mimes.forEach((obj)=>{
+          obj.value.forEach((val)=>{
+            if(fileSuffix==val) contentType = obj.dest
+          })
+        })
+        res.writeHead(200,{'content-type':contentType})
         res.end(file)
       }catch (e) {
         if(pd) console.log(e.message)
